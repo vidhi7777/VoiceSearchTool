07371c1c1ab4cd0b793ec736d28e0c46793e45de
mitikasurana <62178061+mitikasurana@users.noreply.github.com>
2021-03-31 00:33:21 +0530

    commit on 'master'

---
+++ backend/tokens.py
@@ -0,0 +1,21 @@
+import nltk, requests
+
+def get_tokens(cmd_string):
+    tokenizer = nltk.tokenize.RegexpTokenizer('\w+')
+    stopwords = nltk.corpus.stopwords.words("english")
+    additional_sw = ['wear', 'wearing', 'fashion'] #add more relevant terms
+    stopwords.extend(additional_sw)
+
+    cmd_tokens = tokenizer.tokenize(cmd_string)
+    search_tokens = [x for x in cmd_tokens if x not in stopwords]
+
+    print(search_tokens) #only for display purpose
+    
+    return search_tokens
+
+#we can append each keyword to the base_url and redirect to myntra's search page
+# base_url = "https://www.myntra.com/"
+# for w in words:
+#     base_url=base_url+w+"-"
+
+# response = requests.get(base_url)
