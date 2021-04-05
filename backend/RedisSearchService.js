1a0cb886f82b8d39c0079a5ea12be59299f9e160
Sushi22 <sushmitajoshi22@gmail.com>
2021-04-05 20:21:18 +0530

    configured env var

--- backend/RedisSearchService.js
+++ backend/RedisSearchService.js
@@ -1,8 +1,11 @@
+const dotenv = require('dotenv');
+
+dotenv.config()
+
 const dataset = require('./testData').dataset;
 const redis = require("redis");
-const redisPassword = "sushiBot12"
 const rediSearchClient = require('./index.js');
-const my_data = rediSearchClient(redis,'myntra',{ clientOptions:{port: 6379, host: "127.0.0.1",no_ready_check: true,password:redisPassword }});
+const my_data = rediSearchClient(redis,'myntra',{ clientOptions:{port: 6379, host: "127.0.0.1",no_ready_check: true,password:process.env.PASSWORD }});
 
 
 const searchService = () => {
@@ -23,6 +26,7 @@ const searchService = () => {
                 }
             })
     });
+    console.log(my_data);
     my_data.search('kurta',function(err,results){
         if(err){
             console.log(err);
