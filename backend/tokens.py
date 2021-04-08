b0a89c3aadebcd2360a1d3fa1e541877e1f2aabf
mitikasurana <62178061+mitikasurana@users.noreply.github.com>
2021-04-08 13:19:28 +0530

    Updated NLTK and added Spacy tokenizer

--- backend/tokens.py
+++ backend/tokens.py
@@ -1,25 +1,16 @@
-import nltk, requests
+import nltk
+from nltk.tag import pos_tag
+
 nltk.download('stopwords')
 
 def get_tokens(cmd_string):
-    tokenizer = nltk.tokenize.RegexpTokenizer('\w+')
+    
     stopwords = nltk.corpus.stopwords.words("english")
-    additional_sw = ['wear', 'wearing', 'fashion'] #add more relevant terms
+    additional_sw = ['wear', 'wearing', 'fashion', 'clothes', 'outfit'] #add more relevant terms
     stopwords.extend(additional_sw)
 
-    cmd_tokens = tokenizer.tokenize(cmd_string)
-    search_tokens = [x for x in cmd_tokens if x not in stopwords]
-
-    print(search_tokens) #only for display purpose
+    cmd_tokens = pos_tag(nltk.word_tokenize(cmd_string))
+    noun_tokens = [word for word,pos in cmd_tokens if pos == 'NN']
     
-    return search_tokens
-
-#we can append each keyword to the base_url and redirect to myntra's search page
-# base_url = "https://www.myntra.com/"
-# for w in words:
-#     base_url=base_url+w+"-"
-
-# response = requests.get(base_url)
-
-query = input()
-get_tokens(query)
+    return noun_tokens
+    
