bcf86294e86619d0ed1f84dfb8a9b0947633e2b5
Sushi22 <sushmitajoshi22@gmail.com>
2021-04-08 16:15:32 +0530

    test data for UI

---
+++ frontend/userscreens/src/testdata.js
@@ -0,0 +1,37 @@
+[
+    {
+        title : "SOJANYA Men Black &amp; White Solid Kurta with Pyjamas",
+        images : ["http://assets.myntassets.com/v1/assets/images/9228901/2019/4/10/24bc4d8c-62d4-469c-8980-370b0f4cc2a81554891245304-Sojanya-Since-1958-Mens-Cotton-Black-Kurta--White-Pyjama-Set-1.jpg",
+        "http://assets.myntassets.com/v1/assets/images/9228901/2019/4/10/24bc4d8c-62d4-469c-8980-370b0f4cc2a81554891245304-Sojanya-Since-1958-Mens-Cotton-Black-Kurta--White-Pyjama-Set-1.jpg",
+        "http://assets.myntassets.com/v1/assets/images/9228901/2019/4/10/24bc4d8c-62d4-469c-8980-370b0f4cc2a81554891245304-Sojanya-Since-1958-Mens-Cotton-Black-Kurta--White-Pyjama-Set-1.jpg"],
+        body : "Black and white solid kurta with pyjamas Black straight knee length kurta"
+    },
+    {
+        title : "SOJANYA Men Black &amp; White Solid Kurta with Pyjamas",
+        images : ["http://assets.myntassets.com/v1/assets/images/9228901/2019/4/10/24bc4d8c-62d4-469c-8980-370b0f4cc2a81554891245304-Sojanya-Since-1958-Mens-Cotton-Black-Kurta--White-Pyjama-Set-1.jpg",
+        "http://assets.myntassets.com/v1/assets/images/9228901/2019/4/10/24bc4d8c-62d4-469c-8980-370b0f4cc2a81554891245304-Sojanya-Since-1958-Mens-Cotton-Black-Kurta--White-Pyjama-Set-1.jpg",
+        "http://assets.myntassets.com/v1/assets/images/9228901/2019/4/10/24bc4d8c-62d4-469c-8980-370b0f4cc2a81554891245304-Sojanya-Since-1958-Mens-Cotton-Black-Kurta--White-Pyjama-Set-1.jpg"],
+        body : "Black and white solid kurta with pyjamas Black straight knee length kurta"
+    },
+    {
+        title : "SOJANYA Men Black &amp; White Solid Kurta with Pyjamas",
+        images : ["http://assets.myntassets.com/v1/assets/images/9228901/2019/4/10/24bc4d8c-62d4-469c-8980-370b0f4cc2a81554891245304-Sojanya-Since-1958-Mens-Cotton-Black-Kurta--White-Pyjama-Set-1.jpg",
+        "http://assets.myntassets.com/v1/assets/images/9228901/2019/4/10/24bc4d8c-62d4-469c-8980-370b0f4cc2a81554891245304-Sojanya-Since-1958-Mens-Cotton-Black-Kurta--White-Pyjama-Set-1.jpg",
+        "http://assets.myntassets.com/v1/assets/images/9228901/2019/4/10/24bc4d8c-62d4-469c-8980-370b0f4cc2a81554891245304-Sojanya-Since-1958-Mens-Cotton-Black-Kurta--White-Pyjama-Set-1.jpg"],
+        body : "Black and white solid kurta with pyjamas Black straight knee length kurta"
+    },
+    {
+        title : "SOJANYA Men Black &amp; White Solid Kurta with Pyjamas",
+        images : ["http://assets.myntassets.com/v1/assets/images/9228901/2019/4/10/24bc4d8c-62d4-469c-8980-370b0f4cc2a81554891245304-Sojanya-Since-1958-Mens-Cotton-Black-Kurta--White-Pyjama-Set-1.jpg",
+        "http://assets.myntassets.com/v1/assets/images/9228901/2019/4/10/24bc4d8c-62d4-469c-8980-370b0f4cc2a81554891245304-Sojanya-Since-1958-Mens-Cotton-Black-Kurta--White-Pyjama-Set-1.jpg",
+        "http://assets.myntassets.com/v1/assets/images/9228901/2019/4/10/24bc4d8c-62d4-469c-8980-370b0f4cc2a81554891245304-Sojanya-Since-1958-Mens-Cotton-Black-Kurta--White-Pyjama-Set-1.jpg"],
+        body : "Black and white solid kurta with pyjamas Black straight knee length kurta"
+    },
+    {
+        title : "SOJANYA Men Black &amp; White Solid Kurta with Pyjamas",
+        images : ["http://assets.myntassets.com/v1/assets/images/9228901/2019/4/10/24bc4d8c-62d4-469c-8980-370b0f4cc2a81554891245304-Sojanya-Since-1958-Mens-Cotton-Black-Kurta--White-Pyjama-Set-1.jpg",
+        "http://assets.myntassets.com/v1/assets/images/9228901/2019/4/10/24bc4d8c-62d4-469c-8980-370b0f4cc2a81554891245304-Sojanya-Since-1958-Mens-Cotton-Black-Kurta--White-Pyjama-Set-1.jpg",
+        "http://assets.myntassets.com/v1/assets/images/9228901/2019/4/10/24bc4d8c-62d4-469c-8980-370b0f4cc2a81554891245304-Sojanya-Since-1958-Mens-Cotton-Black-Kurta--White-Pyjama-Set-1.jpg"],
+        body : "Black and white solid kurta with pyjamas Black straight knee length kurta"
+    }
+]
