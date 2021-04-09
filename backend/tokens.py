05674319b2e752a4fb3060e374941b911b06b651
Sushi22 <sushmitajoshi22@gmail.com>
2021-04-09 12:38:35 +0530

    logic for integrating node and python

--- backend/tokens.py
+++ backend/tokens.py
@@ -1,7 +1,11 @@
+import sys
 import nltk
 from nltk.tag import pos_tag
 
 nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+
 
 def get_tokens(cmd_string):
     
@@ -11,6 +15,13 @@ def get_tokens(cmd_string):
 
     cmd_tokens = pos_tag(nltk.word_tokenize(cmd_string))
     noun_tokens = [word for word,pos in cmd_tokens if pos == 'NN']
-    
-    return noun_tokens
-    
+
+    result = []
+
+    for token in noun_tokens:
+        token = token+" "
+        result.append(token)
+        
+    print("".join(result))
+
+get_tokens(str(sys.argv[1]))
