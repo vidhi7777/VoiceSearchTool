f6fdc81090657017b27972f20702d6a56802edb9
Sushi22 <sushmitajoshi22@gmail.com>
2021-04-09 12:37:09 +0530

    created py client for running nlp script

---
+++ backend/utils/SimilarWords.js
@@ -0,0 +1,9 @@
+const words= {
+
+    "interview" : ["formal", "trousers", "white shirt","Midi","suit"],
+    "wedding" : ["party", "lehenga", "embroidered"],
+    "picnic" : ["tunics", "shorts", "tops", "jumpsuit"],
+    "Rajasthani" : ["bandhni", "choli", "dhoti","salwar-kameez"]
+}
+
+module.exports.words = words;
